f50bd473d4a7fc0e99e56dd955ede27d8ca2ce77
Edoardo Merli <merliedoardo@gmail.com>
2023-07-09 14:18:56 +0200

    Updated docker launcher

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
-if ! docker info > /dev/null 2>&1; then
+if [ $# -lt 2 ]; then
+    echo "2 arguments expected, $# were given"
+    echo "Usage: `basename "$0"` instance_file_path method"
+    read -n 1 -s -r -p "Press any key to continue"
+    exit 1
+fi
+
+if [ ! docker info > /dev/null 2>&1 ]; then
   echo "This script uses docker, and it isn't running - please start docker and try again!"
-  sleep 3
+  read -n 1 -s -r -p "Press any key to continue"
   exit 1
 fi
 
 if [[ "$(docker images -q mcp 2> /dev/null)" == "" ]]; then
     docker build -t mcp . 
 fi
-docker run --mount type=bind,source="$(pwd)/res",target=/src/res -e "instance_file=$1" -e "method=$2" mcp
-sleep 5
+echo "Starting run of instance $1 using method $2"
+docker run --mount type=bind,source="$(pwd)/res",target=/src/res -e "instance_file=$1" -e "method=$2" --log-driver none mcp
+read -n 1 -s -r -p "Press any key to continue"
+
